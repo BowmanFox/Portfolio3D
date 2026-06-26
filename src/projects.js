c1cb2f0099a51c3a302bb96ac3d6a0d51ca94add
BowmanFox <awesomeguy3612+bowman@gmail.com>
2026-06-26 05:54:45 -0500

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -127,7 +127,7 @@ export const PROJECTS = [
     specs: [
       'Low poly',
       'Semi-detailed',
-      'Meant to fit roblox's art blocky aesthetic',
+      'Meant to fit robloxs art blocky aesthetic',
       'No PBR textures',
     ],
     how: 'I started with the stock, almost gave up, then worked on the barrel. By hour two, I got the barrel done,, the reciever, and was working on the pump. By hour 3, I was working on the finite detail like the grip, and stock again. And, I was done.',
