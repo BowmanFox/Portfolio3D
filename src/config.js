919ca97ea22bacf419510ab05f438a135552b60a
BowmanFox <awesomeguy3612+bowman@gmail.com>
2026-07-18 15:00:26 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -20,5 +20,5 @@ export const CONFIG = {
   // NOTE: requires the WebLLM pin in brain.js to be ≥0.2.80 (gemma3 ids
   // don't exist in older registries).
  // llmModel: 'gemma3-1b-it-q4f16_1-MLC',
-    llmModel: 'gemma3-1b-it-q4f16_1-MLC'',
+    llmModel: 'gemma3-1b-it-q4f16_1-MLC',
 };
